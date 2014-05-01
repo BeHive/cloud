c99656781113ff2b3bc72ac604f2930ec3ac0482
BeHive <behive@me.com>
2014-05-01 18:03:35 +0100

    Login logo

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,18 +1,5 @@
 $(document).ready(function(){
-	$("#logout").off("click").on("click",function(){
-		$("#loader").fadeIn();
-		$.ajax({
-			type: "POST",
-			url: "cloud.php?action=doLogout",
-			success: function(data,status,hdr){
-						$("#loader").fadeOut();
-						$("#pageContent").fadeOut();
-						$("#pageContent").empty();
-						$("#pageContent").html(data);
-						$("#pageContent").fadeIn();
-					}
-		}).done(function(){$("#loader").fadeOut();});
-	});
+	$("#logout").off("click").on("click",doLogout);
 	
 
 });
