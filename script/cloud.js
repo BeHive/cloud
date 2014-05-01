5da6485db579ff90ac78a7354090d5e17df992f0
BeHive <behive@me.com>
2014-05-01 04:53:05 +0100

    login works

--- script/cloud.js
+++ script/cloud.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-	$("#pageContent").load("cloud.php");
 	
 	var opts = {
 		lines: 13, // The number of lines to draw
@@ -21,8 +20,65 @@ $(document).ready(function(){
 	};
 	var target = document.getElementById('loader');
 	var spinner = new Spinner(opts).spin(target);
-
+	
+	$("#login").off("click").on("click",doLogin);
+	$("input").off("keydown").on("keydown",function(){
+		$(this).parent().removeClass("error");
+		$(this).parent().removeAttr("data-tooltip");
+	});
+	
+	$("#loginForm").keydown(function(event){
+    	if(event.keyCode == 13){
+    	    $("#login").click();
+		}
+	});
+	$(".cloudLogo").off("click").on("click",function(){
+		$(".cloudLogo").fadeOut();
+		setTimeout(function() { $("#loginForm").fadeIn(); }, 400);
+	})
 })
+
+function doLogin(){
+
+	$(".error").removeAttr("data-tooltip").removeClass("error");
+	
+	if($("#username").val().length == 0){
+		$("#username").parent().attr("data-tooltip","mandatory");
+		$("#username").parent().addClass("error");
+	}
+	if($("#password").val().length == 0){
+		$("#password").parent().attr("data-tooltip","mandatory");
+		$("#password").parent().addClass("error");
+	}	
+	
+	if($("#username").val().length != 0 && $("#password").val().length != 0){
+		$("#loginForm").fadeOut();
+		$("#loader").fadeIn();
+		$.ajax({
+			type: "POST",
+			url: "cloud.php?action=doLogin",
+			data: { 
+				username: $("#username").val(), 
+				password: $("#password").val()
+			},
+			success: function(data,status,hdr){
+				if(hdr.getResponseHeader("CLOUD-Status") == "error"){
+					$("#login").addClass("error").attr("data-tooltip",hdr.getResponseHeader("CLOUD-Message"));
+					$("#username").val("");
+					$("#password").val("");
+					$("#loginForm").fadeIn();
+				}
+				else{
+					$("#pageContent").hide();
+					$("#loader").fadeOut();
+					$("#pageContent").html(data);
+					$("#pageContent").show("slide",{direction:'right'},1000);
+				}
+				
+			}
+		}).done(function(){$("#loader").fadeOut();});
+	}
+}
 
 function logout(){
 	$("#loader").fadeIn();
