028e9645fbe35c2e72e6f182a2b884a1e5e183e3
BeHive <behive@me.com>
2014-05-01 04:42:34 +0100

    login works

--- main.php
+++
@@ -1,9 +0,0 @@
-
-<link rel="stylesheet" type="text/css" href="css/main.css">
-
-<div id="topMenu" style="display: none;">
-<span id="userName"><?php echo $_SESSION['USERNAME'] ?></span>
-<span class="btn-blue" id="logout">Logout</span>
-</div>
-
-<script src="script/main.js"></script>
