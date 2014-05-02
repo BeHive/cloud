5a649b4b8d381e4482190088ae317a95d3fb15c2
BeHive <behive@me.com>
2014-05-02 06:37:59 +0100

    stable session; dashboard menu structure

--- dashboard.php
+++ dashboard.php
@@ -1,14 +1,35 @@
+<? php
+session_start();
+global $settings;
+?>
 
 <link rel="stylesheet" type="text/css" href="css/dashboard.css">
 
 <div class="gridSmall menuBar">
+	<div id="progressbar"></div>
 	<div id="userData">
 		<span id="userPicture"/>
 		<span id="userName">Hi <?php echo $_SESSION['SHORTNAME'] ?></span>
 		<span id="logout" data-tooltip="logout"/>
 	</div>
-	
+	<ul id="menu">
+		<li class = "divider"></li>
+		<li class="item">item 1</li>
+		<li class = "divider"></li>
+		<li class="item">item 2</li>
+		<li class = "divider"></li>
+		<li class="item">item 3</li>
+		<li class = "divider"></li>
+		<li class="item">item 4</li>
+		<li class = "divider"></li>
+	</ul>
 </div>
-<div class="gridFull dashboardContent"></div>
+<div class="gridFull dashboardContent">
+	<span class="cloudBack">cloud</span>
+</div>
 
 <script src="script/dashboard.js"></script>
+
+<script>
+setTimer(<?php echo $_SESSION['SESSIONTIMEOUT'] ?>);
+</script>
