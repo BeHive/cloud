05d4cf935a18ed75a0036fa1e7c0cbc62df6fb97
BeHive <behive@me.com>
2014-05-02 06:37:49 +0100

    stable session; dashboard menu structure

--- script/dashboard.js
+++ script/dashboard.js
@@ -1,6 +1,37 @@
+var sessionTimer;
+var timerVal;
+
 $(document).ready(function(){
+	// initializing timer bar
+	$("#progressbar").progressbar({ value: 96 });
+	
+	// setting it to decrease every second
+	sessionTimer = setInterval(updateTimer, 1000);
+	
+	// logout button setup
 	$("#logout").off("click").on("click",doLogout);
 	
-
+	// menu item events
+	$("#menu .item").off("click").on("click",function(){
+		$("#menu .item").removeClass("active");
+		$(this).addClass("active");
+	});
 });
 
+function updateTimer(){
+	
+	var timer = $("#progressbar").progressbar("value") - (100 / timerVal);
+
+	if(timer <= 0){
+		clearInterval(sessionTimer);
+		//doLogout();
+	}
+	else
+		$("#progressbar").progressbar({ value: timer });
+
+}
+
+function setTimer(seconds){
+	timerVal = seconds;
+}
+//# sourceURL = dashboard.js
